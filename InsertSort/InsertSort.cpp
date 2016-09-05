df8f8444bb6e2a54bbff52650bea3d84932c0179
Abdirahman <a.osman@mst.edu>
2016-09-04 22:14:53 -0500

    InsertSort is done, only non-essential changes left i.e. aesthetics and readability changes.

--- InsertSort/InsertSort.cpp
+++ InsertSort/InsertSort.cpp
@@ -5,8 +5,13 @@
  *
  * Purpose:  Implementation of Insertion Sort the worst sorting algorithm.
  *
- * Issues:  I want to have the print function be able to show each change that
- * Insert_Sort()'s makes to the array. besides that I'm done.
+ * Issues:  print() should be overloaded so that there is a function for 
+ * handling color prints and there should be one for handling printing the
+ * whole array.
+ *          On anothre note there shoul be a funcs.h and funcs.cpp so that
+ * the function for printing arrays can be more easily used in future
+ * programmes that use them in this series. Or I could not and get more
+ * practice.
  *
  */
 #include<ncurses.h>
@@ -35,14 +40,15 @@ void Insert_Sort(int* A, int n);
 int* rand_array(int n);
 
 /*
- * Precondition: n > 0
+ * Precondition: n > 0 and i>=0
  * 
  * Postcondition: Prints array to stdscr
  *
- * Description: Prints each element of an array to stdscr
+ * Description: Prints each element of an array to stdscr unless the i!=n then
+ * the element at index i is printed to stdscr.
  *
  */
-void print(int* A, int n);
+void print(int* A, int n, int i, short color);//should overload this function
 
 
 int main()
@@ -51,7 +57,10 @@ int main()
   int size=0;
   srand(time(NULL));
   initscr();
-  
+  start_color();
+  init_pair(1, COLOR_BLUE,COLOR_BLACK);
+  init_pair(2, COLOR_RED,COLOR_BLACK);
+  init_pair(3, COLOR_GREEN,COLOR_BLACK);
   printw("What size array: ");
   refresh();
 
@@ -64,6 +73,7 @@ int main()
   clear(); 
 
   //print(Array, size);
+  attroff(A_BLINK);
   Insert_Sort(Array, size);
   delete Array;
 
@@ -93,36 +103,45 @@ void Insert_Sort(int* A, int n)
   int key=0;
   int i;
 
-  print(A,n);
+  print(A,n,n,0);
 
   for(int j=1; j<n; j++ ) 
   {
     key=A[j];
     i=j-1;
-
+    print(A,n,j,2);
     while(i>=0 && A[i] > key)
     {
       A[i+1]=A[i];
+      print(A,n,i,1);//printing new location of element. in blue.
       i--;
-      print(A,i);
     }
+    print(A,n,n,0);//printing whole list. no valid color pair chosen.
     A[i+1]=key;
-    print(A,i+1);
+    print(A,n,i+1,3);//printing key at new index. in green.
   }
-  print(A,n);
+  print(A,n,n,0);
   return;
 }
 
 
-void print(int* A, int n)
+void print(int* A, int n, int i, short color)
 {
-  move(LINES/2,(COLS)/2-2*n);
-  for(int i=0; i<n; i++)
+  //move(LINES/2,(COLS)/2-2*n);
+  if(i!=n)
   {
-    /*mv*/printw(/*LINES/2,(COLS/2)+i,*/"[%d]",A[i]);
-    refresh();
+    attron(COLOR_PAIR(color));
+    mvprintw(LINES/2,i*5,"[%3d]",A[i]);// i*5 each index takes up five colomns.
+    attroff(COLOR_PAIR(1));
   }
-
+  else
+  {
+    for(int k=0; k<n; k++)
+    {
+      mvprintw(LINES/2,k*5,"[%3d]",A[k]);
+    }
+  }
+ refresh();
  usleep(100000);
  return;
 }
